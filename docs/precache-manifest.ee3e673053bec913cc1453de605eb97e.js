8958468b51a6176cf0fbb9888e4ae977fc121e99
Nahuel <vnahuelnicolas@gmail.com>
2020-08-21 17:43:25 -0300

    v1.0 complete

---
+++ docs/precache-manifest.ee3e673053bec913cc1453de605eb97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d992c3587fe4a980f712d4401e6c34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a5958173212ec78d5f",
+    "url": "/static/css/main.22c95052.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a5958173212ec78d5f",
+    "url": "/static/js/main.26731d0a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
